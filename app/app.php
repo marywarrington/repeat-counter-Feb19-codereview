0142f313791c6b838fd56a41426995edfa2a1c66
Mary Warrington <marywarrington@gmail.com>
2016-02-18 13:43:01 -0800

    add results twig and link back to home

--- app/app.php
+++ app/app.php
@@ -13,12 +13,14 @@
             "form" => true
         ));
     });
-    
-    $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig', array(
-            "head" => true,
-            "form" => true
-        ));
+
+    $app->get("/results", function() use ($app) {
+        $my_repeatCounter = new RepeatCounter;
+        $word = $_GET['word'];
+        $sentence = $_GET['sentence'];
+        $result = $my_repeatCounter->countRepeats($word, $sentence);
+        return $app['twig']->render('results.html.twig', array('results' => $result, 'word' => $word, 'sentence' => $sentence)
+        );
     });
 
     return $app;
