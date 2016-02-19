8ddb1b8d1246e5df4e0d4fcfe6460959baa3f23e
Mary Warrington <marywarrington@gmail.com>
2016-02-18 16:03:59 -0800

    Add spec for removing punctuation

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,15 +4,20 @@
 
         function countExplode($input1, $input2)
         {
-            return $explodedArray = explode(" ", $input2);
+            $explodedArray = explode(" ", $input2);
+            foreach ($explodedArray as $index => $word) {
+                $explodedArray[$index] = preg_replace('/[^a-z]/i', '', $word);
+            }
+            return $explodedArray;
         }
 
         function countRepeats($input1, $input2)
         {
             $count = 0;
             $explodedArray = explode(" ", $input2);
-            foreach ($explodedArray as $word) {
-                if ($word == $input1) {
+            foreach ($explodedArray as $index => $word) {
+                $explodedArray[$index] = preg_replace('/[^a-z]/i', '', $word);
+                if ($explodedArray[$index] == $input1) {
                     $count = $count +1;
                 }
             }
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -3,12 +3,25 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
+
         function test_countExplode_explodeString()
         {
             // Arrange
             $test_RepeatCounter = new RepeatCounter;
             $input1 = "here";
             $input2 = "here is a string";
+            // Act
+            $result = $test_RepeatCounter->countExplode($input1, $input2);
+            // Assert
+            $this->assertEquals(array("here", "is", "a", "string"), $result);
+        }
+
+        function test_countExplode_removeNonAlpha()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $input1 = "anything";
+            $input2 = "here!! is: a@ string!";
             // Act
             $result = $test_RepeatCounter->countExplode($input1, $input2);
             // Assert
@@ -36,6 +49,21 @@
             $test_RepeatCounter = new RepeatCounter;
             $input1 = "love";
             $input2 = "I love love love zebras";
+            $input3 = "zebras";
+            // Act
+            $result1 = $test_RepeatCounter->countRepeats($input1, $input2);
+            $result2 = $test_RepeatCounter->countRepeats($input3, $input2);
+            // Assert
+            $this->assertEquals(3, $result1);
+            $this->assertEquals(1, $result2);
+        }
+
+        function test_countRepeats_withPunc()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $input1 = "love";
+            $input2 = "I love, love, !!!love zebras!!!!";
             $input3 = "zebras";
             // Act
             $result1 = $test_RepeatCounter->countRepeats($input1, $input2);
